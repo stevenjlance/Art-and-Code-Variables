5098566a8eb7a14afc724fa9cafca5ec3caf2568
StevenLance <44912347+stevenjlance@users.noreply.github.com>
2021-09-07 18:36:55 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Declare your own variables here
+
+
+function setup() {
+  createCanvas(600, 400);
+	
+}
+
+function draw() {
+  background(180);
+	text(width, 40, 40);
+	text(height, 40, 60);
+
+	// Create your code below this line
+	
+	
+	
+}
